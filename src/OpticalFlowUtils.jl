74ebee561acfe6e329cd3471e4c489ebd28562e8
Stephan Hilb <stephan@ecshi.net>
2021-08-15 11:52:51 +0200

    remove info about file format registration
    The optical flow file format is registered in `FileIO` as of
    JuliaIO/FileIO.jl@08e348c2b4d3b5c8969e3f9b23e81af9577500c0.

--- src/OpticalFlowUtils.jl
+++ src/OpticalFlowUtils.jl
@@ -9,11 +9,4 @@ const FlowField = AbstractArray{<:Union{StaticVector{N},NTuple{N}},N} where N
 include("io.jl")
 include("colorflow.jl")
 
-# TODO: remove once file format has been registered
-@info """Until the *.flo file format is registered in `FileIO` you may want to
-do so yourself:
-    using FileIO: @format_str, add_format
-    add_format(format"FLO", b"PIEH", ".flo", [:OpticalFlowUtils])
-"""
-
 end # module
